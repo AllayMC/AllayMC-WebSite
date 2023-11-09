fc6ac0bbc4928fd232872dfbc98a02e2e3a20225
CoolLoong <1542536763@qq.com>
2023-11-09 19:13:17 +0800

    fix: cant find js css

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import postCssPxToRem from "postcss-pxtorem";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue({
       template: {
