4b74f441a518a8459ee253ab34b4e867a44d7293
Avrilzion <62799045+Avrilzion@users.noreply.github.com>
2024-07-15 20:16:33 +0800

    修复topmenu-menu1/3/4按钮对应跳转 2/5未找到相关内容 (#2)
    * 修复Download和Document按钮跳转
    
    * 修复topmenu-menu1/3/4按钮对应跳转2/5未找到相关内容
    
    * 删除无用无用注释

--- src/components/topmenu.vue
+++ src/components/topmenu.vue
@@ -7,10 +7,10 @@
           <router-link to="/" class="logotext">AllayMC</router-link>
         </div>
         <div id="menu">
-          <a href="#">{{ $t("message.topmenu-menu1") }}</a>
+          <a href="https://docs.allaymc.org">{{ $t("message.topmenu-menu1") }}</a>
           <a href="#">{{ $t("message.topmenu-menu2") }}</a>
-          <a href="#">{{ $t("message.topmenu-menu3") }}</a>
-          <a href="#">{{ $t("message.topmenu-menu4") }}</a>
+          <a href="https://github.com/orgs/AllayMC/discussions">{{ $t("message.topmenu-menu3") }}</a>
+          <a href="https://github.com/orgs/AllayMC/people">{{ $t("message.topmenu-menu4") }}</a>
           <a href="#">{{ $t("message.topmenu-menu5") }}</a>
         </div>
         <div id="right-icon">
